07f53d60401a3ac99fa93cfc3d23ee1b9b4185e1
Abdul Barik <abdulbarik1997m@gmail.com>
2024-12-05 03:20:39 +0600

    Update Cart

--- cart/views.py
+++ cart/views.py
@@ -1,5 +1,6 @@
 from django.views  import generic
-from django.shortcuts import get_list_or_404, redirect
+from django.contrib import messages
+from django.shortcuts import get_object_or_404, redirect
 
 from .carts import Card
 from product.models import Product
@@ -8,7 +9,7 @@ class AddToCart(generic.View):
     def post(self, request, *args, **kwargs):
         product_id = kwargs['product_id']
         product = Product.objects.get(id=product_id)
-        # product = get_list_or_404(Product, id=kwargs.get('product_id'))
+        # product = get_object_or_404(Product, id=kwargs.get('product_id'))
         cart = Card(self.request)
         cart.update(product.id, 1)
         return redirect('cart')
@@ -16,3 +17,30 @@ class AddToCart(generic.View):
     
 class CartItems(generic.TemplateView):
     template_name = 'cart/cart.html'
+    
+    def get(self, request, *args, **kwargs):
+        product_id = request.GET.get('product_id', None)
+        quantity = request.GET.get('quantity', None)
+        clear = request.GET.get('clear', False)
+        cart = Card(request)
+        
+        if product_id and quantity:
+            product = get_object_or_404(Product, id=product_id)
+            if int(quantity) > 0:
+                if product.instock:
+                    cart.update(int(product_id), int(quantity))
+                    return redirect('cart')
+                else:
+                    messages.warning(request, "the Product is not in stock anymore")
+                    return redirect('cart')
+                
+            else:
+                cart.update(int(product_id), int(quantity))
+                return redirect('cart')
+                
+        
+        if clear:
+            cart.clear()
+            return redirect('cart')
+        
+        return super().get(request, *args, **kwargs)
